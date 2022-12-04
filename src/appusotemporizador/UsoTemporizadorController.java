92be746d73775c38d50b5f355f816b07a7238621
Alejandro Santos Gutiérrez <alejandrosantosgut@gmail.com>
2022-12-04 22:56:57 +0100

    Uso componente Temporizador

---
+++ src/appusotemporizador/UsoTemporizadorController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package appusotemporizador;
+
+import componentes_santosalejandro.Temporizador;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Lourdes
+ */
+public class UsoTemporizadorController implements Initializable {
+    
+    @FXML
+    private Label label;
+    @FXML
+    private Temporizador componente1;
+       
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
